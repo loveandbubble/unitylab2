8eee33f89f6378447b73b3a0c2d5151f668c5a72
loveandbubble <natthaporn_yammen@kkumai.com>
2020-12-28 21:50:07 +0700

    spaw

--- Assets/Creator Kit - Beginner Code/Scripts/Tutorial/SpawnerSample.cs
+++ Assets/Creator Kit - Beginner Code/Scripts/Tutorial/SpawnerSample.cs
@@ -1,41 +1,62 @@
-﻿using UnityEngine;
+using UnityEngine;
 using CreatorKitCode;
 
 public class SpawnerSample : MonoBehaviour
 {
     public GameObject ObjectToSpawn;
-
     void Start()
+
     {
+        
+        
         int angle = 15;
         int radius = 5;
         Vector3 spawnPosition = transform.position;
 
         Vector3 direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
         spawnPosition = transform.position + direction * radius;
-        Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
+        //Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
 
         angle = 55;
         direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
         spawnPosition = transform.position + direction * 2;
-        Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
+        //Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
 
         angle = 95;
         direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
         spawnPosition = transform.position + direction * 2;
-        Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
-        
+        //Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
+
         angle = 135;
         direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
         spawnPosition = transform.position + direction * 2;
+        //Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
+        
+    
+        SpawnPotion(0);
+        SpawnPotion(45);
+        SpawnPotion(90);
+        SpawnPotion(135);
+        
+    }
+
+
+    void AddingNumbers(float num1, float num2)
+    {
+        float resultingNumber;
+        resultingNumber = num1 + num2;
+    }
+
+
+
+
+    void SpawnPotion(int angle)
+    {
+        int radius = 5;
+        Vector3 direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
+        Vector3 spawnPosition = transform.position + direction * radius;
         Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
     }
-    
-void AddingNumbers(float num1, float num2)
-{
-  float resultingNumber;
-  resultingNumber = num1 + num2;
-}
-    
-}
 
+
+}
