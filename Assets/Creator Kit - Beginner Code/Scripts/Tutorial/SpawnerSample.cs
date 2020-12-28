1e8a1eae532a4ecc0f5a5df44e5ecb239f3d45c6
loveandbubble <natthaporn_yammen@kkumai.com>
2020-12-29 00:37:30 +0700

    LootAngle Class

--- Assets/Creator Kit - Beginner Code/Scripts/Tutorial/SpawnerSample.cs
+++ Assets/Creator Kit - Beginner Code/Scripts/Tutorial/SpawnerSample.cs
@@ -1,54 +1,23 @@
 using UnityEngine;
 using CreatorKitCode;
 
-public class SpawnerSample : MonoBehaviour
-{
+public class SpawnerSample : MonoBehaviour {
     public GameObject ObjectToSpawn;
-    void Start()
 
-    {
-        
-        
-        int angle = 15;
-        int radius = 5;
-        Vector3 spawnPosition = transform.position;
-
-        Vector3 direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
-        spawnPosition = transform.position + direction * radius;
-        //Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
-
-        angle = 55;
-        direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
-        spawnPosition = transform.position + direction * 2;
-        //Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
-
-        angle = 95;
-        direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
-        spawnPosition = transform.position + direction * 2;
-        //Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
-
-        angle = 135;
-        direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
-        spawnPosition = transform.position + direction * 2;
-        //Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
-        
-    
-        SpawnPotion(0);
-        SpawnPotion(45);
-        SpawnPotion(90);
-        SpawnPotion(135);
-        
+    void Start() {
+        LootAngle myLootAngle = new LootAngle(45);
+        //every call will advance the angle!
+        SpawnPotion(myLootAngle.NextAngle());
+        SpawnPotion(myLootAngle.NextAngle());
+        SpawnPotion(myLootAngle.NextAngle());
+        SpawnPotion(myLootAngle.NextAngle());
     }
-
 
     void AddingNumbers(float num1, float num2)
     {
         float resultingNumber;
         resultingNumber = num1 + num2;
     }
-
-
-
 
     void SpawnPotion(int angle)
     {
@@ -57,6 +26,22 @@ public class SpawnerSample : MonoBehaviour
         Vector3 spawnPosition = transform.position + direction * radius;
         Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
     }
+}
 
+public class LootAngle {
+    int angle;
+    int step;
 
+    LootAngle(int increment)
+    {
+        step = increment;
+        angle = 0;
+    }
+    
+    int NextAngle()
+    {
+        int currentAngle = angle;
+        angle = Helpers.WrapAngle(angle + step);
+        return currentAngle;
+    }
 }
